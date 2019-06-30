382b8549f0c0263549977bd39645ade4ad3a96e1
michalhus <michal.hus@willowtreeapps.com>
2019-06-30 14:29:56 -0400

    Added simple form with input and label that has state and event handling

--- src/componets/SearchBar.js
+++ src/componets/SearchBar.js
@@ -1,13 +1,15 @@
-import React from 'react';
+import React, {useState}  from 'react';
 
 const SearchBar = () => {
-    
+
+    const [searchInput, setSearchInput] = useState([{ term: '' }]);
+
     return (
         <div className='ui segment'>
             <form className='ui form'>
                 <div className='field'>
                     <label>Image Search</label>
-                    <input type='text'></input>
+                    <input type='text' value={searchInput.term} onChange={e => setSearchInput(e.target.value)}></input>
                 </div>
             </form>
         </div>
