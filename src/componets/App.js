f144f8d41f3b5487e4646b1067f61d7e619da37f
michalhus <michal.hus@willowtreeapps.com>
2019-06-30 17:27:35 -0400

    Added axios and small code to make a get API call to the unsplash server

--- src/componets/App.js
+++ src/componets/App.js
@@ -1,11 +1,21 @@
 import React from 'react';
+import axios from 'axios';
 import SearchBar from './SearchBar';
 
 const App = () => {
 
     //This function is using callback, gets variable from the child components
     const onSearchSubmit = (term) => {
-        console.log(term)
+        // console.log(term)
+        //root API location + specific API route
+        axios.get('https://api.unsplash.com/search/photos', {
+            params: {query: term},
+            headers: {
+                Authorization: 
+                    'Client-ID 94b1974dabe9292a5e385f4bba69e1919cc1a3d29d7995a08b8861bb2daec10f'
+            }
+        }
+        );
     }
 
     return(
