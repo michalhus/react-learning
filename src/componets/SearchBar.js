5c02f81047eaff062566f05e349b4a4e34dc6aa6
michalhus <michal.hus@willowtreeapps.com>
2019-06-30 14:09:01 -0400

    Adding SeachBar and main components

---
+++ src/componets/SearchBar.js
@@ -0,0 +1,17 @@
+import React from 'react';
+
+const SearchBar = () => {
+    
+    return (
+        <div className='ui segment'>
+            <form className='ui form'>
+                <div className='field'>
+                    <label>Image Search</label>
+                    <input type='text'></input>
+                </div>
+            </form>
+        </div>
+    );
+}
+
+export default SearchBar
