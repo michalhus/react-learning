02181c920fa79582315e3070e09526bf6707f104
michalhus <michal.hus@willowtreeapps.com>
2019-07-03 18:13:13 -0400

    Last commit for Pic learing app

--- src/componets/App.js
+++ src/componets/App.js
@@ -5,13 +5,15 @@ import ImageList from './ImageList';
 
 const App = () => {
 
+    //State hooks for array from API response
     const [images, setImages] = useState([]);
 
     //This function is using callback, gets variable from the child components
     const onSearchSubmit = async (term) => {
-        //root API location + specific API route
+        //Root API location + Specific API route
         //unsplash.get axios api js file that was create for reusability see imports
         const response = await unsplash.get('/search/photos', {params: {query: term}});
+        //Updates state of images array with response from the API
         setImages(response.data.results)
     }
 
