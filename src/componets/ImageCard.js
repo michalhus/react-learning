e0eaf91b2cc393ecfcc332b4edf500d1e2237934
michalhus <michal.hus@willowtreeapps.com>
2019-07-03 13:52:43 -0400

    added useRef hooks to get attribute value for the element from DOM

--- src/componets/ImageCard.js
+++ src/componets/ImageCard.js
@@ -1,23 +1,60 @@
-import React, {useEffect} from 'react';
+import React, {useEffect, useRef} from 'react';
 
 const ImageCard = (props) => {
 
-    //WIP - need to reverse ENG + read more docs
-    let imageRef = React.createRef();
+    //Sets a referance from DOM - hook use
+    const imageRef = useRef({});
+
+    //Define funcion using const or let with name of the fuction
+    const setSpans = () => {
+        console.log(imageRef.current.clientHeight);
+    }
+    
+
+    useEffect (() => {
+        imageRef.current.addEventListener('load', setSpans);
+    })
+
+ 
+
+
+    // const onButtonClick = () => {
+    //     // `current` points to the mounted text input element
+    //     imageRef.current.clientHeight;
+    // };
+
+    // return (
+    //       <>
+    //         <input ref={imageRef} type="text" />
+    //         <button onClick={onButtonClick}>Focus the input</button>
+    //       </>
+    // ); 
+      
+    console.log(imageRef)
+    
+    // console.log(imageRef.current.addEventListener('load'))
+
+    // console.log(imageRef.current.clientHeight)   // is not loaded yet wait before rendering
+    
+    
+ 
+
+    // setSpans() {
+        
+    // }
 
 
     //Deconstruction of objects/values from the prop
     const {descritpion, urls} = props.image;
 
-    console.log(imageRef.current);
-
     return (
         <div>
-            <img key={props.key} 
+            <img
+            ref={imageRef} 
             alt={descritpion} 
             src={urls.regular} /> 
         </div>
-    )  
+    )
 }
 
 export default ImageCard
